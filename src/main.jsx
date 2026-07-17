82b4cec9604bbebbeec70c87ca0c6fb6bcc8a348
striicksogge <niclas.haufe02@web.de>
2026-07-17 15:19:56 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+
+// HashRouter statt BrowserRouter: GitHub Pages kann nur echte Dateien
+// ausliefern, keine "virtuellen" React-Routen. Mit HashRouter fragt
+// der Browser bei jedem Laden/Neuladen immer nur nach index.html,
+// alles nach dem "#" (z. B. #/recipe/123) regelt React selbst im
+// Browser - kein Server-seitiges Routing-Verständnis nötig.
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
+)
+
